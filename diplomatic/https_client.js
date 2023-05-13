7ebd007752cc67e36d96ddfdfd0073081eb512f5
DaianedaSilva <daiane.supereira@gmail.com>
2023-05-12 23:47:19 -0300

    delete post

--- diplomatic/https_client.js
+++ diplomatic/https_client.js
@@ -193,3 +193,19 @@ export function findUserByNickname(nickname){
   .catch(err => {return err})    
 }
 
+export function deletePostById(postId){
+  return fetch(baseUrl+'post/'+postId,{
+    method: "DELETE",
+    headers: {"Content-type": "application/json; charset=UTF-8"}
+  })
+.then(response =>{
+        if(response.status === 200){
+            return response.json()
+        }else{
+            throw new Error(response.status)
+        }
+
+    }) 
+.then(json => {return json})
+.catch(err => {return err})
+}
